6b488ed84fe92b20a0c93c3ca157d0969836e938
Vadim Aldzhanov <valdjanov@gmail.com>
2023-01-13 17:18:21 +0400

    ok

---
+++ FileWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAndWritePoc
+{
+    internal class FileWriter
+    {
+        public void Write(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ReadAndWritePoc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var readPath = ConfigurationManager.AppSettings["readPath"];
+            var writePath = ConfigurationManager.AppSettings["writePath"];
+            
+            var reader = new FileReader();
+            var text = reader.Read(readPath);
+
+            var writer = new FileWriter();
+            writer.Write(writePath,text);
+        }
+    }
+}
